96e3fb325364e1f5cbd71db58b78f5e76abae736
Jrabbit <jackjrabbit+github@gmail.com>
2012-02-19 16:55:30 -0500

    if not live don't give broken URL

--- ustream.sh
+++ ustream.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-num=$(curl -s  $1 | grep "/mobile/view/channel" | perl -lane 'print $1 if /channel\/(\d+)"/')
-
+num=$(curl -s  $1 | perl -lane 'print $1 if /mobile\/view\/channel\/(\d+)"/')
+if [[ -n num ]]; then
+  echo "Not Live"
+  exit 1
+fi
 echo http://iphone-streaming.ustream.tv/uhls/$num/streams/live/iphone/playlist.m3u8
